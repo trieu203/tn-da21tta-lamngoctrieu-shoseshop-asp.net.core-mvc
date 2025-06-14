7d95e18109b64933102e0559429d3ed9684a852d
trieu203 <trieungocgame123@gmail.com>
2025-06-14 09:00:30 +0700

    update database

--- khoaLuan_webGiay/khoaLuan_webGiay/obj/Debug/net8.0/khoaLuan_webGiay.AssemblyInfo.cs
+++ khoaLuan_webGiay/khoaLuan_webGiay/obj/Debug/net8.0/khoaLuan_webGiay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("khoaLuan_webGiay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51266f8ae0cdff328038da0dfe516b86a02d7676")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7f2119ef29fcb8edbe557fee764c1f032046d2fc")]
 [assembly: System.Reflection.AssemblyProductAttribute("khoaLuan_webGiay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("khoaLuan_webGiay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
