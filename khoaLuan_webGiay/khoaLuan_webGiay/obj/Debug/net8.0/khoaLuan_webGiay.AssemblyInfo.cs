1ccecc992675daef4982d6a227bb5636046b41b1
trieu203 <trieungocgame123@gmail.com>
2025-07-16 17:54:47 +0700

    update word

--- khoaLuan_webGiay/khoaLuan_webGiay/obj/Debug/net8.0/khoaLuan_webGiay.AssemblyInfo.cs
+++ khoaLuan_webGiay/khoaLuan_webGiay/obj/Debug/net8.0/khoaLuan_webGiay.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("khoaLuan_webGiay")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a735076df3ac606ad8d5561427bef32cd50b3b6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd75440551a8f6f636b4c6caa3b6d8e66af5b956")]
 [assembly: System.Reflection.AssemblyProductAttribute("khoaLuan_webGiay")]
 [assembly: System.Reflection.AssemblyTitleAttribute("khoaLuan_webGiay")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
